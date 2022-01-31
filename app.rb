039b4d65d4444b30c2dc0d8fad1a740953829186
huulamnguyen <liamnguyen.swe@gmail.com>
2022-01-31 15:14:02 -0500

    commit tree cc27313ce4c6355147428320f2f16c8af195e1d5

---
+++ app.rb
@@ -0,0 +1,22 @@
+
+# ** Outputting Strings with puts and print
+puts "Hello World!"
+puts "Hello World!"
+puts "Hello World!"
+
+# print "Hello World!"
+# print "Hello World!"
+# print "Hello World!"
+
+print "Pass this test, please."
+
+
+# ** Inspecting Data with p and pp
+# This is the equivalent of calling:
+# puts [1, 2, 3].inspect
+p [1, 2, 3]
+
+# Ruby has one more method, pp, for "pretty-printing" complex data, 
+# such as nested arrays and hashes (the Ruby equivalent of a JavaScript object):
+pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
